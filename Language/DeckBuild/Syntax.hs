55d92b1510e8c616e7a64a24d53ede10a62327bb
Karl Cronburg <karl.cronburg@gmail.com>
2014-11-16 23:04:42 -0500

    more formatting consistency - not sure yet if this is exactly how we want to format haskell

--- Language/DeckBuild/Syntax.hs
+++ Language/DeckBuild/Syntax.hs
@@ -8,30 +8,41 @@ import Data.Generics (Data, Typeable)
 
 data CardType   = TREASURE | ACTION  | VICTORY
     deriving (Eq, Data, Typeable, Show)
+
 data EffectType = MONEY    | ACTIONS | BUYS
     deriving (Eq, Data, Typeable, Show)
+
 type CardID     = String
 
 data Card = Card CardID CardType CardDescr CardCost
     deriving (Eq, Data, Typeable, Show)
-data CardDescr = CardDescr { primary :: [ Effect ]
-                           , other :: String }
-    deriving (Eq, Data, Typeable, Show)
-data Effect = Effect { amount :: Integer
-                     , effectType :: EffectType }
-    deriving (Eq, Data, Typeable, Show)
+
+data CardDescr = CardDescr
+  { primary :: [ Effect ]
+  , other   ::   String
+  } deriving (Eq, Data, Typeable, Show)
+
+data Effect = Effect
+  { amount     :: Integer
+  , effectType :: EffectType
+  } deriving (Eq, Data, Typeable, Show)
+
 type CardCost = Integer
 
 type TurnID = String
 
 data Turn = Turn TurnID [Phase]
-	deriving (Eq, Data, Typeable, Show)
+  deriving (Eq, Data, Typeable, Show)
+
 data Phase = Phase PhaseName PhaseInt
     deriving (Eq, Data, Typeable, Show)
+
 data PhaseInt = PhaseInt Integer | All
 	deriving (Eq, Data, Typeable, Show)
+
 data PhaseName = ActionP  | BuyP  | DiscardP | DrawP 
  	deriving (Eq, Data, Typeable, Show)
+
 -- Do we need this? -- RLV
 -- qName :: QString -> String
 -- qName [n] = n
