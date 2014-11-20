fc0d5f02066304cba1666aaf47273837fd050b8d
Raoul Veroy <rveroy@cs.tufts.edu>
2014-11-20 17:13:25 -0500

    Add the undefined genDeckDecl function.

--- Language/DeckBuild/CodeGen.hs
+++ Language/DeckBuild/CodeGen.hs
@@ -6,5 +6,7 @@ import qualified Language.Haskell.TH as TH
 import Language.DeckBuild.Syntax
 
 make_deck_declaration :: [DeckDecl] -> TH.Q [TH.Dec]
-make_deck_declaration ds = undefined
+make_deck_declaration ds = fmap concat (mapM genDeckDecl ds)
 
+genDeckDecl :: DeckDecl -> TH.Q [TH.Dec]
+genDeckDecl = undefined
