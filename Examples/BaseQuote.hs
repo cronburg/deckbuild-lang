d48cfa6f162a581b100651c90fa0eb557f19e485
mpahrens <Matt.p.ahrens@gmail.com>
2014-11-21 16:51:31 -0500

    added the rest of the cards

--- Examples/BaseQuote.hs
+++ Examples/BaseQuote.hs
@@ -66,6 +66,114 @@ import Language.DeckBuild.Quote
         card Militia :: Action {
           +2 coins
           "Each other player discards down to 3 cards in his hand."
-        }
+        } costs 4
+
+        card Moneynder :: Action {
+          "Trash a Copper from your hand. If you do, +3 coins."
+        } costs 4
+
+        card Remodel :: Action {
+          "Trash a card from your hand. Gain a card costing up to "
+          "2 coins more than the trashed card."
+        } costs 4
+
+        card Smithy :: Action {
+          +3 cards
+        } costs 4
+
+        card Spy :: Action {
+          +1 cards
+          +1 actions
+          "Each player (including you) reveals the top card of his deck "
+          "and either discards it or puts it back, your choice."
+        } costs 4
+
+        card Thief :: Action {
+          "Each other player reveals the top 2 cards of his deck. "
+          "If they revealed any Treasure cards, "
+          "they trash one of them that you choose. "
+          "You may gain any or all of these trashed cards. "
+          "They discard the other revealed cards."
+        } costs 4
+
+        card Throne_Room :: Action {
+          "Choose an Action card in your hand. Play it twice."
+        } costs 4
+
+        card CouncilRoom :: Action {
+          +4 cards
+          +1 buys
+          "Each other player draws a card."
+        } costs 5
+
+        card Festival :: Action {
+          +2 actions
+          +1 buys
+          +2 coins
+        } costs 5
+
+        card Laboratory :: Action {
+          +2 cards
+          +2 actions
+        } costs 5
+
+        card Library :: Action {
+          "Draw until you have 7 cards in hand. "
+          "You may set aside any Action cards drawn this way, "
+          "as you draw them; discard the set aside cards "
+          "after you finish drawing."
+        } costs 5
+
+        card Market :: Action {
+          +1 cards
+          +1 actions
+          +1 buys
+          +1 coins
+        } costs 5
+
+        card Mine :: Action {
+          "Trash a Treasure card from your hand. "
+          "Gain a Treasure card costing up to 3 coins more; "
+          "put it into your hand."
+        } costs 5
+
+        card Witch :: Action {
+          +2 cards
+          "Each other player gains a Curse card."
+        } costs 5
+
+        card Adventurer :: Action {
+          "Reveal cards from your deck until you reveal 2 Treasure cards. "
+          "Put those Treasure cards in your hand "
+          "and discard the other revealed cards."
+        } costs 6
+
+        card Copper :: Treasure {
+          +1 coins
+        } costs 0
+
+        card Silver :: Treasure {
+          +2 coins
+        } costs 3
+
+        card Gold :: Treasure {
+          +3 coins
+        } costs 6
+
+        card Estate :: Victory {
+          +1 victory
+        } costs 2
+
+        card Duchy :: Victory {
+          +3 victory
+        } costs 5
+
+        card Province :: Victory {
+          +6 victory
+        } costs 8
+
+        card Curse :: Victory {
+          -1 victory
+        } costs 0
 
         |]
