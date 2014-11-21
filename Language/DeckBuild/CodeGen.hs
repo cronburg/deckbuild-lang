c9bcfb04cf9867848a3a6e9417fdf2c031d9fc6e
Karl Cronburg <karl.cronburg@gmail.com>
2014-11-21 11:15:43 -0500

    able to create a simple data constructor using quasiquotation.

--- Language/DeckBuild/CodeGen.hs
+++ Language/DeckBuild/CodeGen.hs
@@ -1,12 +1,30 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Language.DeckBuild.CodeGen
   (make_deck_declaration)
   where
 
-import qualified Language.Haskell.TH as TH
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
 import Language.DeckBuild.Syntax
 
-make_deck_declaration :: [DeckDecl] -> TH.Q [TH.Dec]
-make_deck_declaration ds = fmap concat (mapM genDeckDecl ds)
+make_deck_declaration :: [DeckDecl] -> Q [Dec]
+make_deck_declaration ds = do
+--  es <- mapM genDeckExp ds
+--  let body = NormalB $ ListE es
+  let constructors = genCons ds
+  return $
+    [ DataD [] (mkName "Card") [] constructors []
+    ]
+--    [ FunD (mkName "kingdomCards") [Clause [] body []] ]
 
-genDeckDecl :: DeckDecl -> TH.Q [TH.Dec]
-genDeckDecl = undefined
+genCons ds = [NormalC (mkCardName d) [] | d <- ds, isCard d]
+
+isCard (DeckDeclCard c) = True
+isCard _                = False
+
+mkCardName (DeckDeclCard (Card {cID = name})) = mkName name
+mkCardName _ = undefined
+
+--genDeckExp :: DeckDecl -> Q Exp
+--genDeckExp e = undefined --runQ [| e |]
+
