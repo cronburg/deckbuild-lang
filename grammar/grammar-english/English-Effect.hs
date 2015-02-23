f24364a22534a77ade7ad2ab0a53f449b8401cef
Karl Cronburg <karl.cronburg@gmail.com>
2015-02-23 13:51:52 -0500

    English grammar ideas

---
+++ grammar/grammar-english/English-Effect.hs
@@ -0,0 +1,123 @@
+------------------------------------------------------------------------------
+-- Library functions made from base language features:
+
+{- Creates an Event filter function making the given player immune to Effects
+   which affect them: -}
+immune :: Player -> (Event -> Event)
+immune p = (\ e -> if (e affects p) then (Event NULLEVENT) else e)
+
+notimmune :: (Event -> Event)
+notimmune = id
+
+from :: (Cards -> Cards)
+from = id
+
+-- May do something to up to 'n' cards from 'pile' of cards
+mayUpTo ::
+mayUpTo fncn n pile
+  | n <= 0    = return
+  | otherwise = do
+  { didIt <- may fncn (head pile)
+  ; if didIt
+      then mayUpTo fncn (n - 1) (tail pile)
+      else mayUpTo fncn n       (tail pile)
+  }
+
+mayExactly' ::
+mayExactly' fncn n pile
+  | n <= 0     = return (0,pile)
+  | pile == [] = return (0,pile)
+  { didIt <- may fncn (head pile)
+    if didIt
+      then (\(x,y) -> (x+1,y)) 1 $ mayExactly' fncn (n - 1) (tail pile)
+      else (\(x,y) -> (x+1,y)) 0 $ mayExactly' fncn n       (tail pile)
+  }
+
+-- May do something exactly 'n' times from 'pile'
+mayExactly ::
+mayExactly fncn n pile = do
+  { (final_pile,num) <- mayExactly' fncn n pile
+  ; return $ case () of _
+    | num == 0  -> False
+    | num == n  -> True
+    | num <  n  -> mayExactly fncn (n - num) final_pile
+    | otherwise -> undefined -- unreachable code
+  }
+
+-- May ______ up to 'n' cards from 'hand'
+mayDiscardUpTo n hand = mayUpTo discard n hand
+mayTrashUpTo   n hand = mayUpTo trash   n hand
+mayDrawUpTo    n hand = mayUpTo draw    n hand
+
+costingUpTo :: Int -> (Int -> Bool)
+costingUpTo c = (>=) c
+
+-- Gain n cards in the supply satisfying a certain condition:
+gain :: Int -> Cards -> (Card -> Bool) -> ???
+gain n src cndn
+  | n == 0    = ()
+  | otherwise = do
+    { gained <- manymay (gains thisPlayer) (filter cndn src)
+    ; if gained
+        then gain (n - 1) src cndn
+        else gain n src cndn
+    }
+
+getPile p s  = (getNamedPile $ getPlayerName p) ++ s
+getDeck p    = getPile p "'s Deck"
+getDiscard p = getPile p "'s Discard Pile"
+getHand p    = getPile p "'s Hand"
+getTrash     = getNamedPile "Trash"
+
+thisHand   = getHand $ owner thisCard
+thisDeck   = getDeck $ owner thisCard
+thisPlayer = owner thisCard
+
+------------------------------------------------------------------------------
+-- Example effect descriptions for various cards:
+
+-- Cellar:
+{ action { do
+    -- May discard any number of cards from your hand
+  { n <- mayDiscardUpTo (length thisHand) $ from thisHand
+  ; -- Draw the number of cards discarded
+  ; (owner thisCard) `draws` n
+  }
+}
+
+-- Chapel:
+{ action { mayTrashUpTo n (getHand $ owner thisCard) } }
+
+-- Moat:
+{ action   {} -- No special action effect (nothing except +2 cards)
+  reaction {
+    if (((cardTypeOf lastCardPlayed) is "Attack") and       -- When an attack is played
+        (owner lastCardPlayed) != (owner thisCard)) then do -- by someone other than the player holding this card
+      { revealed <- may reveal thisCard                   -- this card may be revealed
+      ; if revealed                                         -- was this card revealed?
+          then return (immune (owner thisCard))             --    TRUE  -> owner of this card is immune to this attack
+          else return notimmune                             --    FALSE -> owner of this card is NOT immune
+      }
+      else return id
+  }
+}
+
+-- Chancellor:
+{ action { do { mayExactly discard (length thisDeck) thisDeck } } }
+
+-- Village:
+{ action {} } -- no extra effects
+-- Woodcutter:
+{ action {} } -- no extra effects
+
+-- Workshop
+{ action { do { gain 1 (from supply) (costingUpTo 4) } } }
+
+-- Bureaucrat
+{ action { do
+  { gains $ fst $ filter (\(Card id _) -> id == "Silver") getSupply
+  ; 
+
+
+
+
---
+++ grammar/grammar-english/effect-grammar.hs
@@ -0,0 +1,43 @@
+-- Keywords / Haskell functions:
+--------------------------------
+
+data Event = Event { 
+data Card  = Card CardID CardType 
+type Cards = [Card]
+
+thisCard     :: Game -> Card         -- gets a ref / copy of the current card
+thisHand     :: Game -> Cards        -- gets a ref / copy of the hand owned by 'thisCard'
+reveal       :: Game -> Card -> Game -- reveals the given card in the Game monad
+discard      :: Game -> Card -> Game -- discards the given card in the Game monad
+playCard     :: Game -> Card -> Game
+draw         :: Game -> Cards -> Game
+
+draws        :: Game -> Player -> Int -> Game
+gains        :: Game -> Player -> Card -> Game
+
+-- returns a list of the top card of each pile in the supply
+getSupply :: Cards
+
+may :: -- ask a player if they want to do something to a specific card
+
+-- ask a player to select 1 or 0 cards from a list of cards
+--   i.e. concurrently do multiple may's
+manymay ::
+
+-- Getters for the various piles of cards in a deck building game:
+getDeck      :: Game -> Player -> Cards
+getDiscard   :: Game -> Player -> Cards
+getHand      :: Game -> Player -> Cards
+getTrash     :: Game -> Cards
+getNamedPile :: Game -> String -> Cards -- 
+getPlayerPile :: Game -> Player -> String -> Cards
+
+getPlayerName :: Game -> Player -> String
+
+------------------------------------------------------------------------------
+-- Only useable by interpreter:
+
+-- A Reaction is a function taking an Event, and returning an altered version of
+-- that event (makes it do something else)
+type Reaction = (Event -> Event)
+
